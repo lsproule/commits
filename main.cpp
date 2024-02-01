50dba5ea201f6097cc13ef471ea7ad83ae44837e
lucas sproule <lucas.sproule.42@gmail.com>
2024-02-01 01:18:37 -0500

    make python script nicer

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 int main() {
-    for (int i = 0; i < 3400; i++) {
+    for (int i = 0; i < 5; i++) {
       std::string  file = static_cast<std::string>("touch new_file/") + std::to_string(i) + static_cast<std::string>(".txt");
       system(file.c_str());
       system("git add .");
--- main.py
+++ main.py
@@ -1,8 +1,42 @@
-from os import system
+from os import system, popen
+from random import randint
 
 
-for x in range(3400):
-    system(f'touch newfile{x}.txt')
+rand = randint(3, 14)
+
+
+def add_commits():
+    for x in range(rand):
+        system(f'touch new_file/newfile{x}.txt')
+        system("git add .")
+        system(f"git commit -m 'new file {x}'")
+        system("git push")
+
+
+def add_issues():
+    system("gh issue create -t 'new issue' -b 'this is a new issue'")
+
+
+def add_pr():
+    system("gh pr create -t 'new pr' -b 'this is a new pr'")
+
+
+def get_pr():
+    pr = popen("gh pr list").read()
+    return pr
+
+
+def merge_pr():
+    get_pr()
+
+
+def clean_up():
+    system("rm -rf new_file/*")
     system("git add .")
-    system(f"git commit -m 'new file {x}'")
+    system("git commit -m 'clean up'")
     system("git push")
+
+
+if __name__ == "__main__":
+    add_commits()
+    clean_up()
